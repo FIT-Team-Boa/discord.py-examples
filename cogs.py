a99f47da1b6277cafa634494f01c02ca5ea83bca
kippiii <ianorzel@gmail.com>
2020-11-16 09:33:14 -0500

    Making the cogs example look nicer

--- cogs.py
+++ cogs.py
@@ -1,27 +1,35 @@
-import discord
+"""Groups commands and event listeners in the bot together using cogs."""
+
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
 
 class Happy(commands.Cog, name="happy_mood"):
+    """A cog that is called 'happy_mood'."""
     def __init__(self, bot):
+        """Initializes the cog."""
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
+        """Prints a message when the bot is ready."""
         print("I am happy!")
 
     @commands.command()
     async def happy(self, ctx):
+        """Responds to a user command."""
         await ctx.send("I am happy! How about you?")
         await self.on_ready()
 
 class Smiley(commands.Cog):
+    """A cog that is called 'Smiley'."""
     def __init__(self, bot):
+        """Initializes the cog."""
         self.bot = bot
 
     @commands.command()
     async def smile(self, ctx):
+        """Returns a message to user after command."""
         await ctx.send(":)")
         happy = self.bot.get_cog("happy_mood")
         if happy is not None:
